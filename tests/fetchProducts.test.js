aef862de488f3ad781cd2d187337ef77fd4fb3f5
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-24 23:43:33 -0300

    adiciona primeiro teste fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,8 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('Testa se fecthProducts é uma funcao', () => {
+    expect(typeof fetchProducts).toBe('function')
+  });
+  
 });
