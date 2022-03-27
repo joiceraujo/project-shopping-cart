21d3d120750aed9b7468fd38a9e4d924902b3628
Joice Araújo <joicearaujoedu@gmail.com>
2022-03-27 12:32:37 -0300

    melhorando testes fetchProducts

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -2,6 +2,8 @@ require('../mocks/fetchSimulator');
 const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
+window.fetch = jest.fn(fetchSimulator);
+
 describe('1 - Teste a função fecthProducts', () => {
   it('Testa se fecthProducts é uma funcao', () => {
     expect(typeof fetchProducts).toBe('function')
@@ -18,13 +20,13 @@ describe('1 - Teste a função fecthProducts', () => {
     expect(fetch).tohavebeencalledWith(endpoint)
   });
 
-  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
-    const result = await fetchProducts('computador')
+  it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', () => {
+    const result = fetchProducts('computador')
     expect(result).toEqual(computadorSearch)
   });
 
-  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', async () => {
-    const result = await fetchProducts()
+  it('Teste se, ao chamar a função fetchProducts sem argumento, retorna um erro com a mensagem: You must provide an url', () => {
+    const result = fetchProducts()
     const erro = new Error('You must provide an url')
     expect(result).toEqual(erro)
   });
